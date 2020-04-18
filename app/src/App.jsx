4f3fb6ede1f58ec3fb77599c7285ac4aceef12ae
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-04-18 15:18:50 +0200

    done

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,45 +1,104 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback, useRef } from "react";
+import produce from "immer";
+
 import "./App.css";
 
-const createRow = number => {
-  const row = [];
-  let i = 0;
-  while (i < number) {
-    row.push(false);
-    i++;
+const numRows = 40;
+const numCols = 90;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+];
+
+const generate = () => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
   }
-  return row;
+
+  return rows;
 };
 
 function App() {
-  const [grid, setGrid] = useState(createRow(5000));
-  const [found, setFound] = useState(false)
+  const [running, setRunning] = useState(false);
+  const [grid, setGrid] = useState(generate());
 
-  const activeCeils = idx => {
-    const newGrid = [...grid];
-    newGrid[idx] = !grid[idx];
-    setGrid(newGrid);
-  };
-  
-  const heart = () => console.log("RUN!")
+  const runningRef = useRef();
+  runningRef.current = running;
 
-  found && setInterval(() => {
-        heart()
-      }, 1000);
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    setGrid(value => {
+      return produce(value, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += value[newI][newK];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (value[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      });
+    });
+
+    setTimeout(runSimulation, 200);
+  }, []);
 
   return (
     <>
-    <h1>GAME THE LIVE</h1>
-    <div className="App">
-      {grid.map((elm, idx) => (
-        <div
-          className={grid[idx] ? "active" : "inactive"}
-          key={idx}
-          onClick={() => activeCeils(idx)}
-        />
-      ))}
-    </div>
-      <button onClick={() => setFound(!found)}>{found ? "STOP":"START"}</button>
+      <div className="header">
+        <h1 className="title">GAME OF LIFE</h1>
+        <button className="button"
+          onClick={() => {
+            setRunning(!running);
+            if (!running) {
+              runningRef.current = true;
+              runSimulation();
+            }
+          }}
+        >
+          {running ? "STOP" : "START"}
+        </button>
+      </div>
+
+      <div
+        className="App"
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              className={col ? "active" : "inactive"}
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+            />
+          ))
+        )}
+      </div>
     </>
   );
 }
